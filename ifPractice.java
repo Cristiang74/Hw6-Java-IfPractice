2fff1a44bb12f3c1aa89f01493fcb6c90fa88f33
Cristian Garcia <cg7497@gmail.com>
2020-10-11 21:38:48 -0500

    Hw6_IfStatements

---
+++ ifPractice.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+/**
+ * ifPractice
+ */
+public class ifPractice {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        
+        // System.out.println("Enter Student's Grade (0-100): ");
+        // int studentGrade = input.nextInt();
+        
+
+    //     if (studentGrade >= 90){
+    //         System.out.println("You got an A! \n");
+    //     }
+    //     else if (studentGrade >= 80 && studentGrade < 90){
+    //         System.out.println("You got a B! \n");
+    //     }
+    
+    //     else if (studentGrade >= 70 && studentGrade < 80){
+    //         System.out.println("You got a C \n");
+    //     }
+    
+    //     else if (studentGrade >= 60 && studentGrade < 70){
+    //         System.out.println("You got a D \n");
+    //     }
+    
+    //     else {
+    //         System.out.println("You Failed, you'll get it next time.\n");
+    //     }
+
+
+
+    // }
+   
+   
+
+    // Switch statement section     
+    // switch(studentGrade/10){
+    //     case 10: 
+    //     System.out.println("Great Job! You got a 100");
+    //     break;
+
+    //     case 9: 
+    //     System.out.println("You got an A!");
+    //     break;
+
+    //     case 8: 
+    //     System.out.println("You got a B");
+    //     break;
+
+    //     case 7: 
+    //     System.out.println("You got a C");
+    //     break;
+
+    //     case 6: 
+    //     System.out.println("You got a D");
+    //     break;
+
+    //     default:
+    //     System.out.println("I'm sorry but you failed.");
+              
+    // }
+
+
+    //Nested if Statement
+
+    int x = 30;
+    int y = 10;
+
+      if( x == 30 ) {
+         if( y == 10 ) {
+            System.out.println("X = 30 and Y = 10");
+         }
+      }
+
+   
+      
+
+    }
+}
+    
+
